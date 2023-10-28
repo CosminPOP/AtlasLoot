167311b7126ecd344e86fdece7aa0b3a99bea263
Lexie <6937574+Lexiebean@users.noreply.github.com>
2023-10-29 07:07:30 +1100

    Version bump

--- Core/AtlasLoot.lua
+++ Core/AtlasLoot.lua
@@ -68,8 +68,8 @@ local AL = AceLibrary("AceLocale-2.2"):new("AtlasLoot");
 
 --Establish version number and compatible version of Atlas
 local VERSION_MAJOR = "1";
-local VERSION_MINOR = "0";
-local VERSION_BOSSES = "9";
+local VERSION_MINOR = "1";
+local VERSION_BOSSES = "0";
 ATLASLOOT_VERSION = "|cffFF8400AtlasLoot TW Edition v"..VERSION_MAJOR.."."..VERSION_MINOR.."."..VERSION_BOSSES.."|r";
 ATLASLOOT_CURRENT_ATLAS = "1.12.0";
 ATLASLOOT_PREVIEW_ATLAS = "1.12.1";
